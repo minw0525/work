e29c0a36e52b9cd830c0b1007fc7594814c048e6
minw0525 <minw0525@naver.com>
2020-12-20 04:42:15 +0900

    색변하게//유니코드없는애 배제
    ㅠ

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+String.prototype.fillZero = function(width){
+    return this.length >= width ? this:new Array(width-this.length+1).join('0')+this;//남는 길이만큼 0으로 채움
+}
+
 async function make(){
     const font = await opentype.load('https://files.cargocollective.com/c891600/Hesiod-Regular.otf');
     const glyphsData = font.glyphs.glyphs
@@ -9,18 +13,22 @@ async function make(){
     console.log(glyphList.length)
     for(char of glyphList){
         writeOnHTML(char)
+        //console.log(char)
     }
     document.querySelector('#totalGlyphs').textContent = `All Characters (${glyphList.length})`
 }
 function writeOnHTML(char){
+    //console.log(char)
     const underbar = /_*/gm;
-    let charTxt = char[1].unicode
+    let charTxt = char[1].unicode;
+    let f;
     if(charTxt === undefined){
-        charTxt = char[1].name.replace(underbar, '')
+        f = false
     }else{
         charTxt = String.fromCharCode(char[1].unicode)
     }
-   // console.log(charTxt)
+    if(f === false) return
+    // console.log(charTxt)
     const charDiv = document.createElement('div');
     charDiv.setAttribute('class','charDiv');
     charDiv.textContent = charTxt;
@@ -50,11 +58,76 @@ document.addEventListener('click',(e)=>{
 })
 make();
 
-document.querySelectorAll('textarea').forEach((el)=>console.log(el))
+
+const szChange = function(){
+    let target = document.querySelector('#text');
+    var val = document.querySelector('#size').value;
+    target.style.fontSize = val+'px';
+    document.querySelector('#curSz').innerHTML = val + 'px';
+    target.style.height = "0px";
+    target.style.height = target.scrollHeight+'px';
+}   
+const htChange = function(){
+    var val = document.querySelector('#lineHeight').value;
+    document.querySelector('#text').style.lineHeight = val;
+    document.querySelector('#curHt').innerHTML = val;
+}   
+
+const spChange = function(){
+    var val = document.querySelector('#spacing').value;
+    document.querySelector('#text').style.letterSpacing = val+'px';
+    document.querySelector('#curSp').innerHTML = val + 'px';
+}
+let colorIndex = 0;
+const colorShft = function(){
+    colorIndex++;
+    console.log(colorIndex)
+    if(colorIndex === 0){
+    document.documentElement.style.setProperty('--color','white');
+    document.documentElement.style.setProperty('--bgColor','black');
+    }else if(colorIndex===1){
+        document.documentElement.style.setProperty('--color','black');
+        document.documentElement.style.setProperty('--bgColor','white');
+    }else {
+        function getRandomBg(){
+            let randomR =  (Math.floor(Math.random()*122)).toString(16).fillZero(2);
+            let randomG =  (Math.floor(Math.random()*122)).toString(16).fillZero(2);
+            let randomB =  (Math.floor(Math.random()*122)).toString(16).fillZero(2);
+            //console.log(`#${randomR+randomB+randomG}`)
+            return `#${randomR+randomB+randomG}`
+        }
+        function getRandomCol(){
+            let randomR =  (Math.floor(Math.random()*122)+123).toString(16).fillZero(2);
+            let randomG =  (Math.floor(Math.random()*122)+123).toString(16).fillZero(2);
+            let randomB =  (Math.floor(Math.random()*122)+123).toString(16).fillZero(2);
+            //console.log(`#${randomR+randomB+randomG}`)
+            return `#${randomR+randomB+randomG}`
+        }
+        document.documentElement.style.setProperty('--color', getRandomCol());
+        document.documentElement.style.setProperty('--bgColor',getRandomBg());
+        if (colorIndex === 6) {colorIndex = -1}
+    }
+}
+
+
+const dlig = function(){
+    var txt = $('textarea');
+    txt.toggleClass('dlig');
+}
+
+document.querySelectorAll('textarea').forEach((el)=>{
+   //console.log(el)
+    //console.log(el.scrollHeight)
+    //console.log(el.style.height)
+    let h = el.scrollHeight;
+    el.style.height = h+'px';
+})
 
 function auto_grow(element) {
     let h = element.scrollHeight;
-    console.log(h)
-    //element.style.height = (element.scrollHeight)+"px";
+    element.style.height = "0px";
+    element.style.height = (element.scrollHeight)+"px";
 }
-
+document.querySelector('#toMain').addEventListener('click',(el)=>{
+    window.scrollTo(0,0);
+})

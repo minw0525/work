84fbde644ecf678ffbde3b77b89f5abeda39a43c
minw0525 <minw0525@naver.com>
2020-12-20 17:40:37 +0900

    모바일 미디어쿼리/스크립트수정
    휴

--- script.js
+++ script.js
@@ -22,23 +22,11 @@ function writeOnHTML(char){
     const charName = document.createElement('div');
     charName.setAttribute('class','charname');
     document.querySelector('#glyphs').appendChild(charDiv);
-    charDiv.addEventListener('click',(e)=>{
-        e.stopPropagation();
-        const txt = e.target.firstChild.textContent;
-        const nameTxt = e.target.lastChild.textContent;
-        const modal = document.querySelector('.modal');
-        const char = document.querySelector('.char');
-        const name = document.querySelector('.name');
-        char.textContent = txt;
-        name.textContent = nameTxt;
-        modal.style.display = 'flex';
-    })
 
     //console.log(char)
     const underbar = /_*/gm;
     const afterDot = /\.[\d|\D]*/gm;
     let charTxt = char[1].unicode;
-    let f = false;
     if(charTxt === undefined){
         charTxt = char[1].name.replace(underbar,'')
         charTxt = charTxt.replace(afterDot,'')
@@ -58,15 +46,29 @@ function writeOnHTML(char){
             default:
                 break;
         }
-        charDiv.style.fontFeatureSettings = "'salt' on"
+        charDiv.classList.add('salt');
     }else{
         charTxt = String.fromCharCode(char[1].unicode)
     }
-    if(f) return
     charDiv.textContent = charTxt;
-    charName.textContent = char[1].name;
+    charName.textContent = char[1].name.replace(afterDot,'');
     charDiv.appendChild(charName);
-
+    charDiv.addEventListener('click',(e)=>{
+        e.stopPropagation();
+        const txt = e.target.firstChild.textContent;
+        const nameTxt = e.target.lastChild.textContent;
+        const modal = document.querySelector('.modal');
+        const char = document.querySelector('.char');
+        const name = document.querySelector('.name');
+        char.textContent = txt;
+        name.textContent = nameTxt;
+        modal.style.display = 'flex';
+        console.log(e.target)
+        if(e.target.classList.contains('salt')){
+            console.log('yes')
+            char.classList.add('salt')
+        }
+    })
 
 }
 document.addEventListener('click',(e)=>{
@@ -83,7 +85,7 @@ const szChange = function(i){
     const target = document.querySelectorAll('textarea')[i];
     let val = document.querySelectorAll('[data-control=size]')[i].value;
     target.style.fontSize = val+'px';
-    document.querySelector('.curSz').innerHTML = val + 'px';
+    document.querySelectorAll('.curSz')[i].innerHTML = val + 'px';
     target.style.height = "0px";
     target.style.height = target.scrollHeight+'px';
 }   
@@ -143,6 +145,18 @@ const colorShft = function(){
     }
 }
 window.onload = ()=>{
+    console.log(window.innerWidth)
+    if(window.innerWidth <= 576){
+        document.querySelectorAll('textarea')[0].style.fontSize = "60px"
+        document.querySelectorAll('[data-control=size]')[0].value = "60"
+        document.querySelectorAll('.curSz')[0].textContent = "60px"
+        document.querySelectorAll('textarea')[1].style.fontSize = "40px"
+        document.querySelectorAll('[data-control=size]')[1].value = "40"
+        document.querySelectorAll('.curSz')[1].textContent = "40px"
+        document.querySelectorAll('textarea')[2].style.fontSize = "20px"
+        document.querySelectorAll('[data-control=size]')[2].value = "20"
+        document.querySelectorAll('.curSz')[2].textContent = "20px"
+    }
     document.querySelectorAll('textarea').forEach((el)=>{
         console.log(el)
         el.style.height = '0px';
@@ -152,6 +166,10 @@ window.onload = ()=>{
         console.log(h)
         el.style.height = (el.scrollHeight)+'px';
     })
+    document.querySelector('.modal').addEventListener('click',(e)=>{
+        document.querySelector('.modal').style.display = 'none';
+    })
+
 }
 
 function auto_grow(element) {
